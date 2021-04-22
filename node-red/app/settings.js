06c9fbb3cd453143cfebcdaeeb38a6ae750483d2
Martin Stroebel <martin.stroebel91@gmail.com>
2021-04-22 21:35:10 +0200

    [refactor] check if changes are stable

--- node-red/app/settings.js
+++ node-red/app/settings.js
@@ -34,15 +34,7 @@ module.exports = {
     serialReconnectTime: 15000,
 
     // Retry time in milliseconds for TCP socket connections
-    //socketReconnectTime: 10000,
-
-    // Timeout in milliseconds for TCP server socket connections
-    //  defaults to no timeout
-    //socketTimeout: 120000,
-
-    // Timeout in milliseconds for HTTP request connections
-    //  defaults to 120 seconds
-    //httpRequestTimeout: 120000,
+    socketReconnectTime: 10000,
 
     // The maximum length, in characters, of any message sent to the debug sidebar tab
     debugMaxLength: 1000,
@@ -52,7 +44,7 @@ module.exports = {
 
     // To enabled pretty-printing of the flow within the flow file, set the following
     //  property to true:
-    //flowFilePretty: true,
+    flowFilePretty: true,
 
     // By default, credentials are encrypted in storage using a generated key. To
     // specify your own secret, set the following property.
@@ -60,7 +52,7 @@ module.exports = {
     // Note: once you set this property, do not change it - doing so will prevent
     // node-red from being able to decrypt your existing credentials and they will be
     // lost.
-    //credentialSecret: "a-secret-key",
+    credentialSecret: "root",
 
     // By default, all user data is stored in the Node-RED install directory. To
     // use a different location, the following property can be used
@@ -70,33 +62,9 @@ module.exports = {
     // The following property can be used to specify an additional directory to scan.
     nodesDir: '/data/node-red/nodes/',
 
-    // By default, the Node-RED UI is available at http://localhost:1880/
-    // The following property can be used to specifiy a different root path.
-    // If set to false, this is disabled.
-    //httpAdminRoot: '/admin',
-
-    // Some nodes, such as HTTP In, can be used to listen for incoming http requests.
-    // By default, these are served relative to '/'. The following property
-    // can be used to specifiy a different root path. If set to false, this is
-    // disabled.
-    //httpNodeRoot: '/red-nodes',
-
-    // The following property can be used in place of 'httpAdminRoot' and 'httpNodeRoot',
-    // to apply the same root to both parts.
-    //httpRoot: '/red',
-
-    // When httpAdminRoot is used to move the UI to a different root path, the
-    // following property can be used to identify a directory of static content
-    // that should be served at http://localhost:1880/.
-    //httpStatic: '/home/nol/node-red-static/',
-
-    // The maximum size of HTTP request that will be accepted by the runtime api.
-    // Default: 5mb
-    //apiMaxLength: '5mb',
-
     // If you installed the optional node-red-dashboard you can set it's path
     // relative to httpRoot
-    //ui: { path: "ui" },
+    ui: { path: "ui" },
 
     // Securing Node-RED
     // -----------------
@@ -121,59 +89,8 @@ module.exports = {
     //httpNodeAuth: {user:"user",pass:"$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN."},
     //httpStaticAuth: {user:"user",pass:"$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN."},
 
-    // The following property can be used to enable HTTPS
-    // See http://nodejs.org/api/https.html#https_https_createserver_options_requestlistener
-    // for details on its contents.
-    // See the comment at the top of this file on how to load the `fs` module used by
-    // this setting.
-    //
-    //https: {
-    //    key: fs.readFileSync('privatekey.pem'),
-    //    cert: fs.readFileSync('certificate.pem')
-    //},
-
-    // The following property can be used to cause insecure HTTP connections to
-    // be redirected to HTTPS.
-    //requireHttps: true
-
-    // The following property can be used to disable the editor. The admin API
-    // is not affected by this option. To disable both the editor and the admin
-    // API, use either the httpRoot or httpAdminRoot properties
-    //disableEditor: false,
-
-    // The following property can be used to configure cross-origin resource sharing
-    // in the HTTP nodes.
-    // See https://github.com/troygoode/node-cors#configuration-options for
-    // details on its contents. The following is a basic permissive set of options:
-    //httpNodeCors: {
-    //    origin: "*",
-    //    methods: "GET,PUT,POST,DELETE"
-    //},
-
-    // If you need to set an http proxy please set an environment variable
-    // called http_proxy (or HTTP_PROXY) outside of Node-RED in the operating system.
-    // For example - http_proxy=http://myproxy.com:8080
-    // (Setting it here will have no effect)
-    // You may also specify no_proxy (or NO_PROXY) to supply a comma separated
-    // list of domains to not proxy, eg - no_proxy=.acme.co,.acme.co.uk
-
-    // The following property can be used to add a custom middleware function
-    // in front of all http in nodes. This allows custom authentication to be
-    // applied to all http in nodes, or any other sort of common request processing.
-    //httpNodeMiddleware: function(req,res,next) {
-    //    // Handle/reject the request, or pass it on to the http in node by calling next();
-    //    // Optionally skip our rawBodyParser by setting this to true;
-    //    //req.skipRawBodyParser = true;
-    //    next();
-    //},
-
     // Anything in this hash is globally available to all functions.
     // It is accessed as context.global.
-    // eg:
-    //    functionGlobalContext: { os:require('os') }
-    // can be accessed in a function block as:
-    //    context.global.os
-
     functionGlobalContext: {
         process: process,
         os: require('os'),
@@ -189,11 +106,11 @@ module.exports = {
     //paletteCategories: ['subflows', 'input', 'output', 'function', 'social', 'mobile', 'storage', 'analysis', 'advanced'],
     editorTheme: {
         page: {
-            title: "balena Node-RED",
+            title: "HeizoPi",
             favicon: "/usr/src/app/assets/favicon.ico"
         },
         header: {
-            title: "balena Node-RED",
+            title: "HeizoPi",
             image: "/usr/src/app/assets/logo.png", // or null to remove image
             url: "https://balena.io"
         },
